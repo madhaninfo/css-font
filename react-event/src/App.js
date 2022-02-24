eba3be20e80b50ebef6e5251260e0543db91bacc
madhaninfo <96818596+madhaninfo@users.noreply.github.com>
2022-02-24 15:38:25 +0530

    commit

---
+++ react-event/src/App.js
@@ -0,0 +1,14 @@
+// parameter passing argument event
+import Football from  "./components/Football";
+
+function App() {
+  const intro="hello"
+  return (
+    <div>
+     <h1>{intro}</h1>
+   <Football/>
+  </div>
+  );
+}
+
+export default App;
---
+++ react-event/src/components/Football.js
@@ -0,0 +1,10 @@
+
+function Football(a) {
+    const shoot =(a) =>
+    alert(a);
+    return (
+     <button onClick={()=> shoot("handsoff !")}>click me</button>
+    )
+}
+
+export default Football;
